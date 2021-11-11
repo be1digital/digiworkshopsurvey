06d3910b462a87662aa9c8507304e91b73b65d6d
cheilrainne <78843426+cheilrainne@users.noreply.github.com>
2021-11-11 15:07:14 +0400

    Base Upload

---
+++ web/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBmeUK7B8ftwx8h44qWUuGdEZQrq3rtWyI",
+  authDomain: "cheilrocks.firebaseapp.com",
+  projectId: "cheilrocks",
+  storageBucket: "cheilrocks.appspot.com",
+  messagingSenderId: "1036559543243",
+  appId: "1:1036559543243:web:4a5f20c25c810ea505244e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
